b36ac494fb4b4f0c0d9825611822ecf03e8f3352
Le5ya <liliatolstyh@yandex.ru>
2022-04-25 20:53:37 +0300

    call funtion

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,7 @@ const modal = () => {
 	const modalBtn = document.querySelector('.modal__button')
 	const modal = document.querySelector('.modal')
 	const modalInner = modal.querySelector('.modal__inner')
-	//const courseButton = document.querySelector('.course__button')
+	const courseButton = document.querySelector('.course__button')
 
 	//const closeImg = document.createElement('div')
 
@@ -52,9 +52,9 @@ const modal = () => {
 	modalBtn.addEventListener('click', () => {
 		modal.style.display = 'flex'
 	})
-	//courseButton.addEventListener('click', () => {
-	//	modal.style.display = 'flex'
-	//})
+	courseButton.addEventListener('click', () => {
+		modal.style.display = 'flex'
+	})
 	modal.addEventListener('click', (event) => {
 		const modalContent = event.target.closest('.modal__inner')
 
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -22,3 +22,4 @@ const smoothScroll = () => {
 	});
 
 }
+smoothScroll()
